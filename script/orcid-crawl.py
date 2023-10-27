f93afc94e2a78815c38d818c3d4f7b3b0c4195bb
Luca Negrini <luca.negrinii@gmail.com>
2023-10-27 13:06:55 +0200

    script refactoring

--- script/orcid-crawl.py
+++ script/orcid-crawl.py
@@ -14,145 +14,164 @@ import hashlib
 import os
 import shutil
 
-def query_api(access_token, user_id, method):
-	return query_path(access_token, '/' + user_id + '/' + method)
+class User:
+	def __init__(self, name, surname, photo, mail, website, role, org, bio, raw_works):
+		self.name = name
+		self.surname = surname
+		self.photo = photo
+		self.mail = mail
+		self.website = website
+		self.role = role
+		self.org = org
+		self.bio = bio
+		self.raw_works = raw_works
 
-def query_path(access_token, path):
-	headers_dict = {
-		'Accept': 'application/vnd.orcid+json',
-		'Authorization':'Bearer ' + access_token
-	}
-	response = requests.get('https://pub.orcid.org/v2.1' + path, headers=headers_dict) 
-	return json.loads(response.text)
+	def has_necessary_fields(self):
+		return self.name is not None and self.surname is not None and self.photo is not None
 
-def parse_user(access_token, user):
-	user_id = user['id']
-	print('Processing', user_id)
-
-	record = query_api(access_token, user_id, 'person')
-	name = record['name']['given-names']['value']
-	surname = record['name']['family-name']['value']
-	bio = record['biography']['content']
-	mail = record['emails']['email'][0]['email']
-	website = record['researcher-urls']['researcher-url'][0]['url']['value']
-
-	try:
-		record = query_api(access_token, user_id, 'activities')
-		role = record['employments']['employment-summary'][0]['role-title']
-		org = record['employments']['employment-summary'][0]['organization']['name']
-	except:
-		# if no employment, this is a student
-		role = 'PhD Student'
-		org = 'Università Ca\' Foscari Venezia'
-
-	return (name, surname, user['photo'], mail, website, role, org, bio), record['works']['group']
-
-def parse_work(access_token, min_date, max_date, work):
-	year = int(work['work-summary'][0]['publication-date']['year']['value'])
-	if work['work-summary'][0]['publication-date']['month'] is not None:
-		month = int(work['work-summary'][0]['publication-date']['month']['value'])
-	else:
-		month = 1
-	if work['work-summary'][0]['publication-date']['day'] is not None:
-		day = int(work['work-summary'][0]['publication-date']['day']['value'])
-	else:
-		day = 1
-	pub_date = date(year, month, day)
-	if pub_date < min_date or pub_date > max_date:
-		return None
-
-	workrecord = query_path(access_token, work['work-summary'][0]['path'])
-	title = workrecord['title']['title']['value']
-	pubtype = workrecord['type']
-	where = workrecord['journal-title']['value']
-	doi = None
-	for extid in workrecord['external-ids']['external-id']:
-		#print(extid)
-		if extid['external-id-type'] == 'doi':
-			doi = extid['external-id-value']
-			break
-	contribs = list()
-	for contributor in workrecord['contributors']['contributor']:
-		contribs += [contributor['credit-name']['value']]
-	return (title, pub_date, pubtype, where, doi, contribs)
-
-def by_date(publication):
-	return publication[1]
-
-def readable(kind):
-	r = kind.lower().replace('_', ' ')
-	if r == 'book':
-		r = 'book chapter'
-	elif r == 'other':
-		r = 'article'
-	return r
-
-def populate_single_person(file, person):
-	print("Adding", person[0], person[1], "to the People page")
-	file.write("""
+	def dump(self):
+		position = f'{self.role} @ {self.org}' if self.role is not None and self.org is not None else ''
+		email = f'Email: <a href="mailto:{self.mail}">{self.mail}</a>' if self.mail is not None else ''
+		website = f'Website: <a href="{self.website}">{self.website}</a>' if self.website is not None else ''
+		bio = f'{self.bio}' if self.bio is not None else ''
+		return f'''
 <div class="div-person-table">
 	<div class="div-person-table">
-		<img class="div-person-table-col" src="{{{{ site.baseurl }}}}/images/{picture}"/>
+		<img class="div-person-table-col" src="{{{{ site.baseurl }}}}/images/{self.photo}"/>
 		<div class="div-person-table-multicol">
-			<h3>{name} {surname}</h3>
-			<h5>{position} @ {location}</h5>
-			Email: <a href="mailto:{mail}">{mail}</a><br/>
-			Website: <a href="{website}">{website}</a>
+			<h3>{self.name} {self.surname}</h3>
+			<h5>{position}</h5>
+			{email}<br/>
+			{website}
 		</div>
 	</div>	
 </div>
 {bio}
 <br/><br/>
 
-""".format(
-	name=person[0], 
-	surname=person[1], 
-	picture=person[2], 
-	mail=person[3], 
-	website=person[4], 
-	position=person[5], 
-	location=person[6], 
-	bio=person[7]))
+'''
 
-def populate_people_page(people, past_people):
-	with open('../people.md', 'w') as file:
-		file.write("""---
-layout: page
-title: People
----
-""")
-		for person in people:
-			populate_single_person(file, person)
-		file.write('## Past members')
-		for person in past_people:
-			populate_single_person(file, person)
+class Pub:
+	def __init__(self, title, pub_date, pub_type, where, doi, contribs):
+		self.title = title
+		self.pub_date = pub_date
+		self.pub_type = pub_type
+		self.where = where
+		self.doi = doi
+		self.contribs = contribs
 
-def add_news(publication):
-	newsname = '{year}-{month}-{day}-paper-{hash}'.format(
-		year=publication[1].year, 
-		month=publication[1].month, 
-		day=publication[1].day, 
-		hash=hashlib.md5(publication[0].encode('utf-8')).hexdigest())
-	print("Generating news for", publication[0], "(", newsname, ")")		
-	with open('../news/_posts/{fname}.md'.format(fname=newsname), 'w') as news:
-		news.write("""---
+	def is_out_of_date_period(self):
+		return self.pub_date is None
+
+	def has_necessary_fields(self):
+		return self.title is not None and self.pub_type is not None and self.where is not None
+
+	def readable_kind(self):
+		r = self.pub_type.lower().replace('_', ' ')
+		if r == 'book':
+			r = 'book chapter'
+		elif r == 'other':
+			r = 'article'
+		return r	
+
+	def dump(self):
+		authors = ', '.join(self.contribs)
+		return f'{authors}: _"{self.title}"_, in {self.where} [[DOI]](https://doi.org/{self.doi})\n\n'
+
+	def to_news_page(self):
+		authors = ', '.join(self.contribs)
+		kind = self.readable_kind()
+		starting_kind = kind.capitalize()
+		return f'''---
 layout: page
-title: '{startingkind} published in "{venue}"'
+title: '{starting_kind} published in "{self.where}"'
 ---
 
 <small>{{{{ page.date | date: "%-d %B %Y" }}}}</small>
 
-The {kind} "{title}", by {authors}, has just been published in "{venue}"! Available [here](https://doi.org/{doi}).
-""".format(
-	authors=', '.join(publication[5]), 
-	title=publication[0], 
-	venue=publication[3], 
-	year=publication[1].year, 
-	month=publication[1].month, 
-	day=publication[1].day, 
-	doi=publication[4], 
-	kind=readable(publication[2]),
-	startingkind=readable(publication[2]).capitalize()))
+The {kind} "{self.title}", by {authors}, has just been published in "{self.where}"! Available [here](https://doi.org/{self.doi}).
+'''
+
+def query_api(access_token, user_id, method, log=False):
+	return query_path(access_token, '/' + user_id + '/' + method, log)
+
+def query_path(access_token, path, log=False):
+	headers_dict = {
+		'Accept': 'application/vnd.orcid+json',
+		'Authorization':'Bearer ' + access_token
+	}
+	response = requests.get('https://pub.orcid.org/v2.1' + path, headers=headers_dict) 
+	if log:
+		print('## Result of querying', path, '##')
+		print(response.text)
+	return json.loads(response.text)
+
+def access_field(accessor, element, field, default=None, log=True):
+	try:
+		el = accessor(element)
+		if log:
+			print('\t' + field + ':', el)
+		return el
+	except Exception as e:
+		print('\tUnable to retrieve value for', field, ', will use default value', default, '. Reason:', str(e))
+		return default
+
+def parse_user(access_token, user):
+	user_id = user['id']
+	print('Processing', user_id)
+
+	record = query_api(access_token, user_id, 'person')
+	name = access_field(lambda r: r['name']['given-names']['value'], record, 'name')
+	surname = access_field(lambda r: r['name']['family-name']['value'], record, 'surname')
+	bio = access_field(lambda r: r['biography']['content'], record, 'bio')
+	mail = access_field(lambda r: r['emails']['email'][0]['email'], record, 'mail')
+	website = access_field(lambda r: r['researcher-urls']['researcher-url'][0]['url']['value'], record, 'website')
+
+	try:
+		record = query_api(access_token, user_id, 'activities')
+		role = access_field(lambda r: r['employments']['employment-summary'][0]['role-title'], record, 'role')
+		org = access_field(lambda r: r['employments']['employment-summary'][0]['organization']['name'], record, 'org')
+	except:
+		# if no employment, this is a student
+		role = 'PhD Student'
+		org = 'Università Ca\' Foscari Venezia'
+
+	raw_works = access_field(lambda r: r['works']['group'], record, 'works', default=list(), log=False)
+	return User(name, surname, user['photo'], mail, website, role, org, bio, raw_works)
+
+def parse_work(access_token, min_date, max_date, work):
+	workrecord = query_path(access_token, access_field(lambda r: r['work-summary'][0]['path'], work, 'work path', log=False))
+	title = access_field(lambda r: r['title']['title']['value'], workrecord, 'title', log=False)
+	print('Processing', title)
+
+	year = int(access_field(lambda r: r['work-summary'][0]['publication-date']['year']['value'], work, 'year'))
+	month = int(access_field(lambda r: r['work-summary'][0]['publication-date']['month']['value'], work, 'month', default=1))
+	day = int(access_field(lambda r: r['work-summary'][0]['publication-date']['day']['value'], work, 'day', default=1))
+	
+	pub_date = date(year, month, day)
+	if pub_date < min_date or pub_date > max_date:
+		# exclude the publication
+		return Pub(None, None, None, None, None, None)
+
+	pub_type = access_field(lambda r: r['type'], workrecord, 'type')
+	where = access_field(lambda r: r['journal-title']['value'], workrecord, 'venue')
+	doi = None
+	for extid in access_field(lambda r: r['external-ids']['external-id'], workrecord, 'external ids', default=list(), log=False):
+		#print(extid)
+		if access_field(lambda r: r['external-id-type'], extid, 'external id type', log=False) == 'doi':
+			doi = access_field(lambda r: r['external-id-value'], extid, 'external id')
+			break
+	contribs = list()
+	for contributor in access_field(lambda r: r['contributors']['contributor'], workrecord, 'contributors', default=list(), log=False):
+		contribs += [access_field(lambda r: r['credit-name']['value'], contributor, 'contributor name')]
+	return Pub(title, pub_date, pub_type, where, doi, contribs)
+
+def add_news(publication):
+	name_hash = hashlib.md5(publication.title.encode('utf-8')).hexdigest()
+	news_name = f'{publication.pub_date.year}-{publication.pub_date.month}-{publication.pub_date.day}-paper-{name_hash}'
+	print('Generating news for', publication.title, '(', news_name, ')')		
+	with open(f'../news/_posts/{news_name}.md', 'w') as news:
+		news.write(publication.to_news_page())
 
 def populate_publications_page(publications):
 	with open('../publications.md', 'w') as file:
@@ -163,20 +182,57 @@ title: Publications
 """)
 		curryear = None
 		for publication in publications:
-			year = publication[1].year
+			year = publication.pub_date.year
 			if curryear is None:
 				curryear = year
-				file.write('## {year}\n\n'.format(year=curryear))
+				file.write(f'## {curryear}\n\n')
 			elif year != curryear:
 				curryear = year
-				file.write('## {year}\n\n'.format(year=curryear))
+				file.write(f'## {curryear}\n\n')
 
-			print("Adding", publication[0], "to the Publications page")
-			file.write('{authors}: _"{title}"_, in {venue} [[DOI]](https://doi.org/{doi})\n\n'.format(
-				authors=', '.join(publication[5]), 
-				title=publication[0], 
-				venue=publication[3], 
-				doi=publication[4]))
+			print("Adding", publication.title, "to the Publications page")
+			file.write(publication.dump())
+
+def populate_people_page(people, past_people):
+	with open('../people.md', 'w') as file:
+		file.write("""---
+layout: page
+title: People
+---
+""")
+		for person in people:
+			print("Adding", person.name, person.surname, "to the People page")
+			file.write(person.dump())
+		file.write('## Past members')
+		for person in past_people:
+			print("Adding", person.name, person.surname, "to the People page")
+			file.write(person.dump())
+
+def process_user_and_add(user, people_list, publications_list):
+	person = parse_user(access_token, user)
+	if person.has_necessary_fields():
+		people_list += [person]
+	else:
+		print('\tUser discarded due to missing fields')
+
+	min_date = user['from']
+	max_date = date.today() if user['to'] == 'today' else user['to']
+	for work in person.raw_works:
+		publication = parse_work(access_token, min_date, max_date, work)
+		if publication.is_out_of_date_period():
+			print('\tPublication discarded since it is outside of the specified date range')
+			continue
+		elif not publication.has_necessary_fields():
+			print('\tPublication discarded due to missing fields')
+			continue
+		elif publication.doi is None or any(publication.doi == pub.doi for pub in publications_list):
+			print('\tPublication discarded due to duplicate doi')
+			continue
+		else:
+			publications_list += [publication]
+
+def sort_by_date(publication):
+	return publication.pub_date
 
 if __name__ == '__main__':
 	with open('users.yaml', 'r') as yamlfile:
@@ -189,40 +245,12 @@ if __name__ == '__main__':
 	publications = list()
 
 	for user in data['users']:
-		person, works = parse_user(access_token, user)
-		print(person)
-		people += [person]
-
-		min_date = user['from']
-		max_date = date.today() if user['to'] == 'today' else user['to']
-		for work in works:
-			publication = parse_work(access_token, min_date, max_date, work)	
-			if publication is None:
-				continue
-			doi = publication[4]
-			if doi is not None and not any(doi == pub[4] for pub in publications):
-				# avoid duplicates
-				print(publication)
-				publications += [publication]
+		process_user_and_add(user, people, publications)
 
 	for user in data['past_users']:
-		person, works = parse_user(access_token, user)
-		print(person)
-		past_people += [person]
+		process_user_and_add(user, past_people, publications)
 
-		min_date = user['from']
-		max_date = date.today() if user['to'] == 'today' else user['to']
-		for work in works:
-			publication = parse_work(access_token, min_date, max_date, work)	
-			if publication is None:
-				continue
-			doi = publication[4]
-			if doi is not None and not any(doi == pub[4] for pub in publications):
-				# avoid duplicates
-				print(publication)
-				publications += [publication]
-
-	publications = sorted(publications, key=by_date)
+	publications = sorted(publications, key=sort_by_date)
 	populate_people_page(people, past_people)
 	populate_publications_page(reversed(publications))
 	
